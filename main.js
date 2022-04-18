a1c1c6aecb313435b7ebd2e3cf727401f661095e
Xica Felix <xicasfelix@gmail.com>
2022-04-18 16:56:31 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//  Step 1 - Click on Heart and activate server response
+function thatCallback(e){
+const targetHeart = e.target;
+mimicServerCall('some URL').then(function (){
+// Step 2 - Success Server Response:
+// If the Heart is Empty: 
+  if (targetHeart.innerHTML === EMPTY_HEART) {
+    // Change the heart to full
+    targetHeart.innerHTML = FULL_HEART;
+    // Add activated class
+    targetHeart.classList.add('activated-Heart');
+  } else {
+    // If the heart is full:
+    // Remove Activated Class
+    targetHeart.removeAttribute('activated-Heart');
+    // Change the Heart to empty
+    targetHeart.innerHTML = EMPTY_HEART;
+  }
+  //  Step 3 - Failed Server Response
+  // Use catch and display error message
+}).catch(function (error){
+  // Display the error message for 3 seconds
+  const theMessage = document.getElementById('modal');
+  theMessage.removeAttribute('hidden');
+  setTimeout(()=> theMessage.className = 'hidden', 3000); 
+})
+}
 
+
+const likeButton = document.getElementsByClassName('like-glyph')
+for (const i of likeButton){
+  i.addEventListener('click',thatCallback)}
 
 
 //------------------------------------------------------------------------------
